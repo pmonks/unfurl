e66c0238cdca44ccd34568fc675cfe33cf5c45c6
Peter Monks <pmonks@gmail.com>
2016-09-13 14:24:15 -0700

    :arrow_up: Add exclusions to avoid donstream conflicts

--- project.clj
+++ project.clj
@@ -23,7 +23,9 @@
   :dependencies     [
                       [org.clojure/clojure "1.8.0"]
                       [clj-http            "3.2.0" :exclusions [org.clojure/clojure]]
-                      [hickory             "0.6.0" :exclusions [org.clojure/clojure]]
+                      [org.jsoup/jsoup     "1.9.2"]
+                      [hickory             "0.6.0" :exclusions [org.clojure/clojure org.jsoup/jsoup]]
+
                     ]
   :profiles         {:dev {:dependencies [[midje      "1.8.3"]]
                            :plugins      [[lein-midje "3.2.1"]]}   ; Don't remove this or travis-ci will assplode!
