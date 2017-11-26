676743f8465df547a8a67cc6d24f3914afa8028c
Peter Monks <pmonks@gmail.com>
2017-11-25 20:57:39 -0800

    :new: Improve character set handling

--- src/unfurl/api.clj
+++ src/unfurl/api.clj
@@ -131,7 +131,7 @@
                                                           :socket-timeout   timeout-ms
                                                           :conn-timeout     timeout-ms
                                                           :headers          {"Range"          (str "0-" (- max-content-length 1))
-                                                                             "Accept-Charset" "UTF-8"}
+                                                                             "Accept-Charset" "UTF-8, ISO-8859-1;q=0.5, *;q=0.1"}
                                                           :client-params    {"http.useragent" user-agent}
                                                           :proxy-host       proxy-host
                                                           :proxy-port       proxy-port}))
