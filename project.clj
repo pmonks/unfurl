1a92965febc9998d21db6720518db573822f86d7
Peter Monks <pmonks@gmail.com>
2019-10-25 09:16:51 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -23,18 +23,18 @@
   :min-lein-version    "2.8.1"
   :repositories        [["sonatype-snapshots" {:url "https://oss.sonatype.org/content/groups/public" :snapshots true}]
                         ["jitpack"            {:url "https://jitpack.io"}]]
-  :dependencies        [[org.clojure/clojure "1.10.0"]
-                        [clj-http            "3.9.1" :exclusions [org.clojure/clojure]]
-                        [org.jsoup/jsoup     "1.11.3"]
+  :dependencies        [[org.clojure/clojure "1.10.1"]
+                        [clj-http            "3.10.0" :exclusions [org.clojure/clojure]]
+                        [org.jsoup/jsoup     "1.12.1"]
                         [hickory             "0.7.1" :exclusions [org.clojure/clojure org.jsoup/jsoup org.clojure/clojurescript viebel/codox-klipse-theme]]]
   :profiles            {:dev  {:plugins      [[lein-licenses "0.2.2"]
-                                              [lein-codox    "0.10.4"]]}
+                                              [lein-codox    "0.10.7"]]}
                         :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
                         :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
                         :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
                         :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
                         :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-                        :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+                        :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :deploy-repositories [["snapshots" {:url      "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password}]
